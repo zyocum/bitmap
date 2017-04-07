d87fd993bf2832301e67c00e402284238dd20014
Zachary Yocum <zachary.yocum@me.com>
2017-04-06 20:07:50 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
 # bitmap
 Search 2D bitmaps for blocks of adjacent cells whose values are 1
+
+##Usage
+	./bitmap.py -h
+	usage: bitmap.py [-h] [-c COLUMN_DELIMITER] [-r ROW_DELIMITER] [-d]
+	
+	Search 2D bitmaps for blocks of adjacent cells whose values are 1
+	
+	optional arguments:
+	  -h, --help            show this help message and exit
+	  -c COLUMN_DELIMITER, --column-delimiter COLUMN_DELIMITER
+	                        column delimiter string (default: None)
+	  -r ROW_DELIMITER, --row-delimiter ROW_DELIMITER
+	                        row delimiter string (default: )
+	  -d, --count-diagonals
+	                        consider diagonal cells to be adjacent; e.g., [0,0]
+	                        and [1,1] would be considered adjacent (default:
+	                        False)
+
+##Examples
+	$ echo "0101
+	0111
+	0101" > bitmap.txt
+  
+Ther are several sample files in the `bitmaps` directory:
+
+	$ for f in bitmaps/*.txt; do
+		./bitmap.py < "$f"
+	done
+	bitmap:
+	10
+	00
+	blocks:
+	[(0, 0)]
+	bitmap:
+	00
+	01
+	blocks:
+	[(1, 1)]
+	bitmap:
+	00000
+	01100
+	01100
+	00000
+	00001
+	blocks:
+	[(1, 1), (1, 2), (2, 2), (2, 1)]
+	[(4, 4)]
+	bitmap:
+	10001
+	01101
+	01101
+	00001
+	00001
+	blocks:
+	[(0, 0)]
+	[(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+	[(1, 1), (1, 2), (2, 2), (2, 1)]
+	bitmap:
+	0101
+	0111
+	0101
+	blocks:
+	[(0, 1), (1, 1), (1, 2), (1, 3), (0, 3), (2, 3), (2, 1)]
+	bitmap:
+	0011111100
+	0100000010
+	0101001010
+	0101001010
+	0100000010
+	0101001010
+	0100110010
+	0100000010
+	0011111100
+	blocks:
+	[(0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7)]
+	[(1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)]
+	[(1, 8), (2, 8), (3, 8), (4, 8), (5, 8), (6, 8), (7, 8)]
+	[(2, 3), (3, 3)]
+	[(2, 6), (3, 6)]
+	[(5, 3)]
+	[(5, 6)]
+	[(6, 4), (6, 5)]
+	[(8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7)]
+
+You can supply the `-d/--count-diagonals` option if you want to include diagonal cells as adjacent to one another:
+
+	$ for f in bitmaps/*.txt; do
+		./bitmap.py < "$f"
+	done
+	bitmap:
+	10
+	00
+	blocks:
+	[(0, 0)]
+	bitmap:
+	00
+	01
+	blocks:
+	[(1, 1)]
+	bitmap:
+	00000
+	01100
+	01100
+	00000
+	00001
+	blocks:
+	[(1, 1), (1, 2), (2, 2), (2, 1)]
+	[(4, 4)]
+	bitmap:
+	10001
+	01101
+	01101
+	00001
+	00001
+	blocks:
+	[(0, 0), (1, 1), (1, 2), (2, 2), (2, 1)]
+	[(0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+	bitmap:
+	0101
+	0111
+	0101
+	blocks:
+	[(0, 1), (1, 2), (0, 3), (1, 3), (2, 3), (2, 1), (1, 1)]
+	bitmap:
+	0011111100
+	0100000010
+	0101001010
+	0101001010
+	0100000010
+	0101001010
+	0100110010
+	0100000010
+	0011111100
+	blocks:
+	[(0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 8), (2, 8), (3, 8), (4, 8), (5, 8), (6, 8), (7, 8), (8, 7), (8, 6), (8, 5), (8, 4), (8, 3), (8, 2), (7, 1), (6, 1), (5, 1), (4, 1), (3, 1), (2, 1), (1, 1)]
+	[(2, 3), (3, 3)]
+	[(2, 6), (3, 6)]
+	[(5, 3), (6, 4), (6, 5), (5, 6)]
---
+++ bitmap.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+"""Search 2D bitmaps for blocks of adjacent cells whose values are 1"""
+
+import numpy as np
+
+def adjacents(bitmap, cell, diagonals=False):
+    """Generate all cells adjacent to the given cell (including itself)"""
+    if not (0, 0) <= cell < bitmap.shape:
+        return []
+    i, j = cell
+    rows, cols = bitmap.shape
+    north = (i-1,j) if 0 < i else None
+    east = (i,j+1) if j < cols - 1 else None
+    south = (i+1,j) if i < rows - 1else None
+    west = (i,j-1) if 0 < j else None
+    cells = [cell, north, east, south, west]
+    if diagonals:
+        northeast = (i-1,j+1) if 0 < i and j < cols - 1 else None
+        southeast = (i+1,j+1) if i < rows - 1 and j < cols - 1 else None
+        southwest = (i+1,j-1) if i < rows - 1 and 0 < j else None
+        northwest = (i-1,j-1) if 0 < i and 0 < j else None
+        cells = [
+            cell, 
+            north, northeast, east, southeast,
+            south, southwest, west, northwest
+        ]
+    return [c for c in cells if c is not None]
+
+def explore(bitmap, cell, block=[], explored=set(), diagonals=False):
+    """Recursively explore a block of adjacent bits whose values are 1"""
+    for adjacent in adjacents(bitmap, cell, diagonals):
+        if adjacent not in explored:
+            explored.add(adjacent)
+            if bitmap[adjacent]:
+                block.append(adjacent)
+                explore(bitmap, adjacent, block, explored, diagonals)
+    return block
+
+def blocks(bitmap, diagonals=False):
+    """Generate all blocks of adjacent cells whose value is 1"""
+    explored = set()
+    for i, row in enumerate(bitmap):
+        for j, col in enumerate(row):
+            cell = i, j
+            if bitmap[cell]:
+                block = explore(
+                    bitmap, cell,
+                    block=[],
+                    explored=explored,
+                    diagonals=diagonals
+                )
+                if block:
+                    yield block
+
+def load(string, col_sep=None, row_sep='\n'):
+    """Load a bitmap as a numpy array from a string
+    
+    load('0101\n0111\n0101') -> numpy.array([
+        [0, 1, 0, 1],
+        [0, 1, 1, 1],
+        [0, 1, 0, 1]
+    ])
+    
+    """
+    bitmap = []
+    for row in string.rstrip().split(row_sep):
+        row = [c for c in row] if col_sep is None else row.split(col_sep)
+        row = list(map(int, row))
+        bitmap.append(row)
+    return np.array(bitmap, dtype=int)
+
+def show(bitmap, **kwargs):
+    """Pretty-print a bitmap and all blocks of adjacent 1s in the bitmap"""
+    print('bitmap:')
+    print(*(''.join(map(str, row)) for row in bitmap), sep='\n')
+    print('blocks:')
+    for block in blocks(bitmap, **kwargs):
+        print(block)
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '-c', '--column-delimiter',
+        default=None,
+        help='column delimiter string'
+    )
+    parser.add_argument(
+        '-r', '--row-delimiter',
+        default='\n',
+        help='row delimiter string'
+    )
+    parser.add_argument(
+        '-d', '--count-diagonals',
+        action='store_true',
+        help=(
+            'consider diagonal cells to be adjacent; e.g., '
+            '[0,0] and [1,1] would be considered adjacent'
+        )
+    )
+    args = parser.parse_args()
+    bitmap = load(
+        sys.stdin.read(),
+        col_sep=args.column_delimiter,
+        row_sep=args.row_delimiter
+    )
+    show(bitmap, diagonals=args.count_diagonals)
