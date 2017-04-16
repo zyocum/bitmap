fcfa31abde0b59370d824fe530ad31b48edf3b9c
Zachary Yocum <zachary.yocum@me.com>
2017-04-16 17:41:48 -0400

    add que-based strategy

--- bitmap.py
+++ bitmap.py
@@ -4,6 +4,8 @@
 
 import numpy as np
 
+from collections import deque
+
 def adjacents(bitmap, cell, diagonals=False):
     """Generate all cells adjacent to the given cell (including itself)"""
     if not (0, 0) <= cell < bitmap.shape:
@@ -14,7 +16,13 @@ def adjacents(bitmap, cell, diagonals=False):
     east = (i,j+1) if j < cols - 1 else None
     south = (i+1,j) if i < rows - 1 else None
     west = (i,j-1) if 0 < j else None
-    cells = [cell, north, east, south, west]
+    cells = [
+        cell,
+        north,
+        east,
+        south,
+        west
+    ]
     if diagonals:
         northeast = (i-1,j+1) if 0 < i and j < cols - 1 else None
         southeast = (i+1,j+1) if i < rows - 1 and j < cols - 1 else None
@@ -37,8 +45,8 @@ def explore(bitmap, cell, block=[], explored=set(), diagonals=False):
                 explore(bitmap, adjacent, block, explored, diagonals)
     return block
 
-def blocks(bitmap, diagonals=False):
-    """Generate all blocks of adjacent cells whose value is 1"""
+def blocks_recursive(bitmap, diagonals=False):
+    """Generate all blocks of adjacent cells whose value is 1 recursively"""
     explored = set()
     for i, row in enumerate(bitmap):
         for j, col in enumerate(row):
@@ -51,7 +59,38 @@ def blocks(bitmap, diagonals=False):
                     diagonals=diagonals
                 )
                 if block:
-                    yield block
+                    yield sorted(block)
+
+def blocks_que(bitmap, diagonals=False):
+    """Generate all blocks of adjacent cells whose value is 1 via a deque"""
+    explored = set()
+    for i, row in enumerate(bitmap):
+        for j, col in enumerate(row):
+            cell = i, j
+            block = deque()
+            if bitmap[cell] and cell not in explored:
+                block.append(cell)
+                while not all(((c in explored) for c in block)):
+                    cell = block.popleft()
+                    explored.add(cell)
+                    for adjacent in adjacents(bitmap, cell, diagonals):
+                        if bitmap[adjacent] and adjacent not in set(block):
+                            block.append(adjacent)
+            explored.add(cell)
+            if block:
+                yield sorted(set(block))
+
+def blocks(bitmap, strategy, **kwargs):
+    """Generate all blocks using the given strategy"""
+    return strategy(bitmap, **kwargs)
+
+def show(bitmap, strategy, **kwargs):
+    """Pretty-print a bitmap and all blocks of adjacent 1s in the bitmap"""
+    print('bitmap:')
+    print(*(''.join(map(str, row)) for row in bitmap), sep='\n')
+    print('blocks:')
+    for block in blocks(bitmap, strategy, **kwargs):
+        print(block)
 
 def load(string, col_sep=None, row_sep='\n'):
     """Load a bitmap as a numpy array from a string
@@ -70,17 +109,13 @@ def load(string, col_sep=None, row_sep='\n'):
         bitmap.append(row)
     return np.array(bitmap, dtype=int)
 
-def show(bitmap, **kwargs):
-    """Pretty-print a bitmap and all blocks of adjacent 1s in the bitmap"""
-    print('bitmap:')
-    print(*(''.join(map(str, row)) for row in bitmap), sep='\n')
-    print('blocks:')
-    for block in blocks(bitmap, **kwargs):
-        print(block)
-
 if __name__ == '__main__':
     import argparse
     import sys
+    STRATEGIES = {
+        'recursive': blocks_recursive,
+        'que': blocks_que
+    }
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
@@ -96,6 +131,11 @@ if __name__ == '__main__':
         help='row delimiter string'
     )
     parser.add_argument(
+        '-s', '--strategy',
+        default='que',
+        choices=set(STRATEGIES.keys())
+    )
+    parser.add_argument(
         '-d', '--count-diagonals',
         action='store_true',
         help=(
@@ -109,4 +149,4 @@ if __name__ == '__main__':
         col_sep=args.column_delimiter,
         row_sep=args.row_delimiter
     )
-    show(bitmap, diagonals=args.count_diagonals)
+    show(bitmap, STRATEGIES[args.strategy], diagonals=args.count_diagonals)
